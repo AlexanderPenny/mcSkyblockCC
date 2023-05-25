294b5bf64766c0a51f2787240e2d02f66d8bd587
AlexanderPenny <124367964+AlexanderPenny@users.noreply.github.com>
2023-05-26 00:53:58 +0100

    Leaderboard lua update [incomplete]
    errors reading files

--- CustomerScreens/leaderboard.lua
+++ CustomerScreens/leaderboard.lua
@@ -1,35 +1,71 @@
 -------------declarations of variables-------------
 local leaderboard = peripheral.wrap("monitor_0")
-leaderboard.setTextScale(3)
 local leaderboardArray = {
     {"CHz01G", "FeR21l", "mNE48r", "QNz23f", "OmX901", "PlK51e", "YrT42P"},
-    {0,0,0,0,0,0,0}
+    {0,0,0,0,0,0,0},
+    {"Ross","Andrey","Matthew","Xander","Sanders","Momo","Struan"}
 }
 
 -------------Functions-------------
 local function readFromFiles()
---read all files into array (idk how files are stored)
+local n = #leaderboardArray[1]
+rednet.open("left")
+
+for i = 1, n do
+    local dataFile = fs.open(leaderboardArray[1][i] ,"r")
+    if dataFile then  -- Check if the file was opened successfully
+        rednet.send(5, leaderboardArray[1][i], "Read")
+        local id, message = rednet.receive()
+        leaderboardArray[2][i] = message
+        dataFile.close()
+    else
+        error("Failed to open file: " .. leaderboardArray[1][i] .. ".txt")
+    end
+end
+
+rednet.close("left")
 end
 
 local function bubbleSort()
-    local n #leaderboardArray[1]
+    local n = #leaderboardArray[1]
 
-    for i = 1, n do
-        for j = 1, n-i
-
+    repeat
+        local swapped = false
+        for i = 1, n - 1 do
+            if leaderboardArray[2][i] > leaderboardArray[2][i + 1] then
+                leaderboardArray[2][i], leaderboardArray[2][i + 1] = leaderboardArray[2][i + 1], leaderboardArray[2][i]
+                swapped = true
+            end
         end
+        n = n - 1
+    until not swapped
+end
+
+local function drawScreen()
+    --initialise
+    leaderboard.clear()
+    local n = #leaderboardArray[1]
+
+    leaderboard.setTextScale(5)
+    leaderboard.write("Richest Gamblers")
+    leaderboard.write("")
+
+    leaderboard.setTextScale(3)
+    for i = 1, n do
+        leaderboard.write(leaderboardArray[3][i] .. ": " .. leaderboardArray[2][i])
     end
+
 end
 
 -------------main loop-------------
 while true do
 
-leaderboard.clear()
-
---read and sort file information
+ --read and sort file information
 readFromFiles()
 bubbleSort()
+drawScreen()
 
-
+-- polls every 15 minutes to be low-performance impacting
+os.sleep(900)
 
 end
