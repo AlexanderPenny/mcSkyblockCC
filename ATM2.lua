2a064918d0b17010ae843fa17513a09bc39c83ce
AlexanderPenny <124367964+AlexanderPenny@users.noreply.github.com>
2023-05-26 21:38:42 +0100

    clickable menu buttons added to ATM2.lua
    - click collisions/checks are slightly dodgy

--- ATM2.lua
+++ ATM2.lua
@@ -8,7 +8,7 @@ local button1, button2, button3, button4
 
 --misc
 local continueLoop = true
-local mouseX, mouseY
+local mouseX, mouseY = 0,0
 --------------------------startup--------------------------
 term.clear()
 rednet.open("back")
@@ -34,27 +34,37 @@ disk.eject("Bottom")
 local function checkClick()
     --check if the mouse has been clicked
     -- Check if the mouse has been clicked
-    local event, button, x, y = os.pullEvent("mouse_click")
+    local event, button, x, y = os.pullEvent("mouse_up")
 
+    --set variables
     mouseX = x
     mouseY = y
+    local width, _ = term.getSize()
     
     if menuMode == 0 then
-        if mouseX > (math.floor((width - #text) / 5 + 1)) and mouseX < (math.floor((width) / 5 + 1)) then
+        if mouseX > (math.floor((width - 13) / 5 + 1)) and mouseX < (math.floor((width) / 5 + 1)) then
             --if here, then it is the left two buttons
-                if mouseY > 6 and mouseY < 7 then
+                if mouseY > 5 and mouseY < 8 then
                     menuMode = 1 -- withdraw money
-                elseif mouseY > 14 and mouseY < 15 then
+                elseif mouseY > 13 and mouseY < 16 then
                     menuMode = 2 -- deposit money
                 end
-            elseif mouseX > (math.floor((width - #text) / 1.3 + 1)) and mouseX < (math.floor((width) / 1.3 + 1)) then
+        elseif mouseX > (math.floor((width - 13) / 1.3 + 1)) and mouseX < (math.floor((width) / 1.3 + 1)) then
             -- if here then it is the right two buttons
-                if mouseY > 6 and mouseY < 7 then
+                if mouseY > 5 and mouseY < 8 then
                     menuMode = 3 -- view balance
-                elseif mouseY > 14 and mouseY < 15 then
+                elseif mouseY > 13 and mouseY < 16 then
                     menuMode = 4 -- transfer money
                 end
-            end 
+        end 
+    elseif menuMode == 1 then
+        
+    elseif menuMode == 2 then
+
+    elseif menuMode == 3 then
+
+    else -- menumMode == 4 (this is done anyway)
+        
     end
 end
 
@@ -63,22 +73,22 @@ local function getTerminalWidth() -- just so that its easier to write
 end
 
 local function writeCentered(text, line)
-    local width, _ = term.getSize()
-    local x = math.floor((width - #text) / 2 + 1)
+    local width, height = term.getSize()
+    local x = math.floor((width - 13) / 2 + 1)
     term.setCursorPos(x, line)
     term.write(text)
 end
 
 local function writeLeft(text, line)
-    local width, _ = term.getSize()
-    local x = math.floor((width - #text) / 5 + 1)
+    local width, height = term.getSize()
+    local x = math.floor((width - 13) / 5 + 1)
     term.setCursorPos(x, line)
     term.write(text)
 end
 
 local function writeRight(text, line)
-    local width, _ = term.getSize()
-    local x = math.floor((width - #text) / 1.3 + 1)
+    local width, height = term.getSize()
+    local x = math.floor((width - 13) / 1.3 + 1)
     term.setCursorPos(x, line)
     term.write(text)
 end
@@ -88,20 +98,36 @@ local function drawScreen()
     term.clear()
 
     if menuMode == 0 then
-    --set text information
-    getTerminalWidth()
-    term.setTextColor(colors.black)
+        --set text information
+        getTerminalWidth()
+        term.setTextColor(colors.black)
 
-    --write text
-    button1 = "[UNAVAILABLE]"
-    button2 = "Check Balance"
-    button3 = "[UNAVAILABLE]"
-    button4 = "Transfer Cash"
-    writeCentered("ATM", 1)
-    writeLeft(button1, 6) -- withdraw money
-    writeRight(button2, 6)
-    writeLeft(button3, 14) -- deposit money
-    writeRight(button4, 14)
+        --write text
+        writeCentered("ATM", 1)
+        writeLeft("Withdraw Cash", 6) -- withdraw money
+        writeRight("Check Balance", 6)
+        writeLeft("Deposit Money", 14) -- deposit money
+        writeRight("Transfer Cash", 14)
+    elseif menuMode == 1 then
+        term.setCursorPos(1,1)
+        term.write("BACK")
+
+        writeCentered("menu unavailable", 10)
+    elseif menuMode == 2 then
+        term.setCursorPos(1,1)
+        term.write("BACK")
+
+        writeCentered("menu unavailable", 10)
+    elseif menuMode == 3 then
+        term.setCursorPos(1,1)
+        term.write("BACK")
+
+        writeCentered("Balance", 1)
+    else -- menuMode == 4 then
+        term.setCursorPos(1,1)
+        term.write("BACK")
+
+        writeCentered("Transfer Cash", 1)
     end
 end
 
@@ -122,3 +148,4 @@ rednet.close()
 
 -------------------notes-------------------
 --NOTE: you cannot set text size on non-advanced terminals
+--all buttons should be 13 characters in length (on the main menu)
